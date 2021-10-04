af83c365025c192bdfcad114972fdcf1fe9a1960
RalkeyOfficial <Ralkey@outlook.com>
2021-10-04 12:52:46 +0200

    pp poopoo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../images/logo.png'
+import './Navbar.css'
 
 function navbar() {
     return (
@@ -7,12 +9,12 @@ function navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo">
-                        <img src="../../public/images/logo.jpg" alt=""></img>
+                        <img src={Logo} alt="Logo" className="logo" />
                     </Link>
                     <ul className="navbar-items">
                         <li><a href="/">item 1</a></li>
                         <li><a href="/">item 2</a></li>
-                        <li><a href="/">item 2</a></li>
+                        <li><a href="/">item 3</a></li>
                     </ul>
                 </div>
             </nav>
